45499b0628e857c9ac558c48b9dbc5b2ecf0e89c
lwpinter <lwpinter@hawaii.edu>
2025-12-07 21:48:21 -1000

    Delete BIOL243-bootstrap directory

--- BIOL243-bootstrap/scripts/script.js
+++
@@ -1,7 +0,0 @@
-// Set the current year in the footer badge.
-(() => {
-    const yearTarget = document.getElementById("year");
-    if (yearTarget) {
-        yearTarget.textContent = new Date().getFullYear();
-    }
-})();
